3a075c6866e028e943cb2a5f3c43104cab392ece
AdrianTP <github@adriantp.com>
2019-11-18 15:02:30 -0600

    updated youtube collapse script for last week's youtube update.

--- youtube.landing-page-collapse-sections.js
+++ youtube.landing-page-collapse-sections.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         YouTube Landing Page Collapsible Sections
 // @namespace    https://github.com/AdrianTP
-// @version      0.2
-// @description  Click landing page section titles to collapse/expand that section. Alt-click to collapse/expand all of them at once.
+// @version      0.3
+// @description  Click landing page section titles to collapse/expand that section.
 // @encoding     utf-8
 // @license      https://creativecommons.org/licenses/by-sa/4.0/
 // @author       https://github.com/AdrianTP
@@ -15,59 +15,44 @@
 // @downloadURL  https://raw.githubusercontent.com/AdrianTP/userscripts/master/youtube.landing-page-collapse-sections.js
 // ==/UserScript==
 
-// TODO: style button
-
 (function() {
   'use strict';
 
   var NAMESPACE = 'adriantp-youtube-landing-collapse',
-    STORAGE_NAME = `${NAMESPACE}-list`,
+    STORAGE_NAME = `${NAMESPACE}-cookie`,
     COLLAPSED_CLASS = `${NAMESPACE}-collapsed`,
-    BUTTON_TEXT_EXPANDED = 'HIDE ALL',
-    BUTTON_TEXT_COLLAPSED = 'SHOW ALL',
-    BUTTON_BAR_SELECTOR = 'ytd-masthead #buttons',
     BROWSE_PARENT_SELECTOR = 'ytd-two-column-browse-results-renderer',
     BROWSE_ID = 'primary',
-    SECTION_PARENT_SELECTOR = 'ytd-section-list-renderer[page-subtype="home"]',
-    SECTION_SELECTOR = 'ytd-shelf-renderer',
-    TITLE_ID = 'title-container',
+    HEADER_ID = 'grid-header',
     CONTENTS_ID = 'contents',
-    buttonParent = null,
-    browseParent = null,
-    sectionParent = null,
     styleTag = document.createElement('style'),
-    button = document.createElement('button'),
-    style = `${SECTION_PARENT_SELECTOR} ${SECTION_SELECTOR} #${TITLE_ID} {
+    style = `#${HEADER_ID} {
       cursor: pointer;
     }
 
-    ${SECTION_PARENT_SELECTOR} ${SECTION_SELECTOR} #${TITLE_ID}:hover {
+    #${HEADER_ID}:hover {
       background: rgba(0, 0, 0, .1);
     }
 
-    ${SECTION_PARENT_SELECTOR} ${SECTION_SELECTOR} #${TITLE_ID} h2::before {
+    #${HEADER_ID} #title::before {
       content: "▾";
       width: 15px;
       margin-top: -3px;
       cursor: pointer;
     }
 
-    ${SECTION_PARENT_SELECTOR} ${SECTION_SELECTOR}.${COLLAPSED_CLASS} #${TITLE_ID} h2::before {
+    .${COLLAPSED_CLASS} #${HEADER_ID} #title::before {
       content: "▸";
     }
 
-    ${SECTION_PARENT_SELECTOR} ${SECTION_SELECTOR}.${COLLAPSED_CLASS} #${CONTENTS_ID} {
-      display: none;
-    }
-
-    ${BROWSE_PARENT_SELECTOR}.${COLLAPSED_CLASS} #${BROWSE_ID} {
-      display: none;
+    .${COLLAPSED_CLASS} #${CONTENTS_ID} {
+      visibility: hidden;
     }`,
-    // taken from: https://stackoverflow.com/q/13452626/771948
-    setCookie = function setCookie(c_name, value, expiredays) {
+    // modified from: https://stackoverflow.com/q/13452626/771948
+    setCookie = function setCookie(name, value = 1, expiredays = null) {
       var exdate = new Date();
       exdate.setDate(exdate.getDate() + expiredays);
-      document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ?
+      document.cookie = name + "=" + escape(value) + ((expiredays == null) ?
         "" :
         ";expires=" + exdate.toUTCString());
     },
@@ -79,47 +64,24 @@
     deleteCookie = function deleteCookie(a) {
       document.cookie = a + '= ; expires = Thu, 01 Jan 1970 00:00:00 GMT';
     },
-    superCollapsed = !! getCookieValue(STORAGE_NAME),
-    loadState = function loadState() {
-      button.innerText = superCollapsed ? BUTTON_TEXT_COLLAPSED : BUTTON_TEXT_EXPANDED;
-      browseParent.classList.toggle(COLLAPSED_CLASS, superCollapsed);
-
-      if (superCollapsed) {
-        setCookie(STORAGE_NAME, 1, null);
-      } else {
-        deleteCookie(STORAGE_NAME);
-      }
-    },
-    toggleAll = function toggleAll(state) {
-      document.querySelectorAll(SECTION_SELECTOR).forEach((el)=>{
-        el.classList.toggle(COLLAPSED_CLASS, state);
-      });
-    },
     toggle = function toggle(e) {
-      var titleElement = e.target.closest(`#${TITLE_ID}`),
-        sectionElement = e.target.closest(SECTION_SELECTOR);
+      var browseWrapper = document.querySelector(`#${BROWSE_ID}`);
 
-      if (!titleElement || !sectionElement) {
-        return true;
+      if (getCookieValue(STORAGE_NAME)) {
+        deleteCookie(STORAGE_NAME);
+      } else {
+        setCookie(STORAGE_NAME);
       }
-
-      sectionElement.classList.toggle(COLLAPSED_CLASS);
-
-      if (e.altKey) {
-        toggleAll(sectionElement.classList.contains(COLLAPSED_CLASS));
-      }
-    },
-    superToggle = function superToggle(e) {
-      browseParent.classList.toggle(COLLAPSED_CLASS);
-
-      superCollapsed = browseParent.classList.contains(COLLAPSED_CLASS);
 
       loadState();
     },
+    loadState = function loadState(){
+      var browseWrapper = document.querySelector(`#${BROWSE_ID}`);
+
+      browseWrapper.classList.toggle(COLLAPSED_CLASS, getCookieValue(STORAGE_NAME));
+    },
     waitForElements = function waitForElements(selectors, cb) {
       var domEls = selectors.map(i => document.querySelector(i)).filter(i => i !== null);
-
-      console.log(domEls.length, selectors.length);
 
       if (domEls.length !== selectors.length) {
         window.requestAnimationFrame(()=>{ waitForElements(selectors, cb); });
@@ -128,21 +90,13 @@
       }
     },
     init = function init() {
-//      GM_addStyle(style); // TODO: figure out why the fuck this doesn't work
-      styleTag.innerHTML = style;
+      styleTag.innerText = style;
       document.head.appendChild(styleTag);
 
-      waitForElements([`${BUTTON_BAR_SELECTOR} ytd-topbar-menu-button-renderer`, BROWSE_PARENT_SELECTOR, SECTION_PARENT_SELECTOR], () => {
+      waitForElements([`#${BROWSE_ID}`, `#${HEADER_ID}`, `#${CONTENTS_ID}`], () => {
         // TODO: consider using MutationObserver instead: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-
-        buttonParent = document.querySelector(BUTTON_BAR_SELECTOR);
-        browseParent = document.querySelector(BROWSE_PARENT_SELECTOR);
-        sectionParent = document.querySelector(SECTION_PARENT_SELECTOR);
-
-        buttonParent.insertAdjacentElement('afterbegin', button);
-
-        button.addEventListener('click', superToggle);
-        sectionParent.addEventListener('click', toggle);
+        // TODO: example with Promises: https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
+        document.querySelector(`#${HEADER_ID}`).addEventListener('click', toggle);
 
         loadState();
       });
